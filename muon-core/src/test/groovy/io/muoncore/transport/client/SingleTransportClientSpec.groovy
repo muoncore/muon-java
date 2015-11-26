aefcc0b6b7687e6ca6a3f43556bbfeed2ce67e00
David Dawson <david.dawson@simplicityitself.com>
2015-11-26 11:06:56 +0000

    Init env for test

--- muon-core/src/test/groovy/io/muoncore/transport/client/SingleTransportClientSpec.groovy
+++ muon-core/src/test/groovy/io/muoncore/transport/client/SingleTransportClientSpec.groovy
@@ -1,11 +1,14 @@
 package io.muoncore.transport.client
 
 import io.muoncore.transport.MuonTransport
+import reactor.Environment
 import spock.lang.Specification
 
 class SingleTransportClientSpec extends Specification {
 
     def "client creates a SingleTransportChannelConnection for every request"() {
+        Environment.initializeIfEmpty()
+
         def transport = Mock(MuonTransport)
         def dispatcher = Mock(TransportMessageDispatcher)
 
