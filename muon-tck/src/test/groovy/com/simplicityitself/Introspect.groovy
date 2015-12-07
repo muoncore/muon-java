b3b45d76c5a4f7be59b5d6aa2882051d50dd2814
David Dawson <david.dawson@simplicityitself.com>
2015-12-07 07:41:55 +0000

    Hack in discovery settle time pending investigation.

--- muon-tck/src/test/groovy/com/simplicityitself/Introspect.groovy
+++ muon-tck/src/test/groovy/com/simplicityitself/Introspect.groovy
@@ -21,11 +21,21 @@ class Introspect {
 
         Thread.sleep(4000)
 
-        println new JsonBuilder(muon.discovery.knownServices).toPrettyString()
+//        println new JsonBuilder(muon.request("request://UserService/addPerson", [
+//                name:"Gawain",
+//                age:25
+//        ], Map).get()).toPrettyString()
+//
+        println new JsonBuilder(muon.request("request://UserService/getPeople", Object).get()).toPrettyString()
+
+
+//        println new JsonBuilder(muon.discovery.knownServices).toPrettyString()
 //        println new JsonBuilder(muon.introspect("service1").get()).toPrettyString()
 //        println new JsonBuilder(muon.request("request://service1/getPeople", List).get().payload).toPrettyString()
 
+        muon.shutdown()
         println "DONE!"
         Thread.sleep(2000)
+
     }
 }
