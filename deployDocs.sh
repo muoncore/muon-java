575da7d50079e08f4c0c2ddb62210086170e3884
David Dawson <david.dawson@simplicityitself.com>
2015-12-15 13:05:47 +0000

    Remove the iml again

--- deployDocs.sh
+++ deployDocs.sh
@@ -23,7 +23,7 @@ git clone git@github.com:microserviceux/muon-documentation.git
 cd  muon-documentation
 mkdir -p java
 rsync -avr --delete ../../doc/ java/$DOCPATH
-rm java/*.iml
+rm java/$DOCPATH/*.iml
 git add java/
 git commit -m "Update Java Documentation"
 git push origin
