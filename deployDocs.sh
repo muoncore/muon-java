031d763db37c2f3e81cd8a6ae4680a60bc08c2b7
David Dawson <david.dawson@simplicityitself.com>
2015-12-15 10:50:00 +0000

    Documentation copying

--- deployDocs.sh
+++ deployDocs.sh
@@ -5,7 +5,7 @@ cd  tempdocumentation
 git clone git@github.com:microserviceux/muon-documentation.git
 cd  muon-documentation
 mkdir -p java
-cp -R ../../doc/* java
+rsync -avr --delete ../../doc/ java/
 git add java/
 git commit -m "Update Java Documentation"
 git push origin
