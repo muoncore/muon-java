4311e452a80270ef49bcf17bbbbea0aac96f3a7a
David Dawson <david.dawson@simplicityitself.com>
2015-12-17 20:18:04 +0000

    Some documentation.

--- doc/src/main/groovy/io/muoncore/example/Introduction2.groovy
+++ doc/src/main/groovy/io/muoncore/example/Introduction2.groovy
@@ -1,10 +1,8 @@
 package io.muoncore.example
-
 import io.muoncore.spring.annotations.EnableMuon
 import io.muoncore.spring.annotations.MuonController
 import io.muoncore.spring.annotations.MuonRequestListener
 import org.springframework.boot.autoconfigure.SpringBootApplication
-
 
 @Grapes([
         @Grab('io.muoncore:muon-transport-amqp:6.4-SNAPSHOT'),
@@ -12,11 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
         @Grab('io.muoncore:muon-spring:6.4-SNAPSHOT')])
 @SpringBootApplication
 @MuonController                        //(1) Enable Muon and set up
-@EnableMuon(serviceName = "users")   //(2)
-class Introduction {
+@EnableMuon(serviceName = "gateway")   //(2)
+class Introduction2 {
 
     @MuonRequestListener(path = "/")   //(3) An RPC Endpoint
     def myRpcEndpoint(Map data) {
         return data
     }
 }
+
+
