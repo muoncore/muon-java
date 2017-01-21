f0eeb3d87019f44d3c51f3d3c78b6aaae300d86d
David Dawson <david.dawson@simplicityitself.com>
2017-01-21 19:40:27 +0100

    add space for auth in sub request

--- muon-core/src/main/java/io/muoncore/protocol/reactivestream/messages/ReactiveStreamSubscriptionRequest.java
+++ muon-core/src/main/java/io/muoncore/protocol/reactivestream/messages/ReactiveStreamSubscriptionRequest.java
@@ -1,28 +1,40 @@
 package io.muoncore.protocol.reactivestream.messages;
 
+import io.muoncore.protocol.Auth;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class ReactiveStreamSubscriptionRequest {
 
-    private String streamName;
-    private Map<String, Object> args = new HashMap<>();
+  private Auth auth;
+  private String streamName;
+  private Map<String, Object> args = new HashMap<>();
 
-    public ReactiveStreamSubscriptionRequest(String streamName) {
-        this.streamName = streamName;
-    }
+  public ReactiveStreamSubscriptionRequest(String streamName, Auth auth) {
+    this.auth = auth;
+    this.streamName = streamName;
+  }
 
-    public String getStreamName() {
-        return streamName;
-    }
+  public ReactiveStreamSubscriptionRequest(String streamName) {
+    this.streamName = streamName;
+  }
 
-    public void arg(String name, String value) {
-        args.put(name, value);
-    }
+  public Auth getAuth() {
+    return auth;
+  }
 
-    public Map<String, Object> getArgs() {
-        return args;
-    }
+  public String getStreamName() {
+    return streamName;
+  }
+
+  public void arg(String name, String value) {
+    args.put(name, value);
+  }
+
+  public Map<String, Object> getArgs() {
+    return args;
+  }
 
 
 }
