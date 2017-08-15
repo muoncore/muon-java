8e9cb5a5508d18b945af09b2fd782b12504f618a
David Dawson <me@daviddawson.me>
2017-08-15 13:28:05 +0100

    Fixes #63
    InMemTransport will fail new channels after triggerFailure is called.

--- muon-core/src/main/java/io/muoncore/memory/transport/InMemTransport.java
+++ muon-core/src/main/java/io/muoncore/memory/transport/InMemTransport.java
@@ -8,71 +8,107 @@ import io.muoncore.channel.support.Scheduler;
 import io.muoncore.codec.Codecs;
 import io.muoncore.config.AutoConfiguration;
 import io.muoncore.exception.MuonTransportFailureException;
+import io.muoncore.message.MuonMessage;
+import io.muoncore.message.MuonMessageBuilder;
 import io.muoncore.protocol.ServerStacks;
 import io.muoncore.transport.MuonTransport;
 import io.muoncore.message.MuonInboundMessage;
 import io.muoncore.message.MuonOutboundMessage;
+import io.muoncore.transport.TransportEvents;
+import lombok.extern.slf4j.Slf4j;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Optional;
 
+@Slf4j
 public class InMemTransport implements MuonTransport {
 
-    private EventBus bus;
-    private AutoConfiguration configuration;
+  private EventBus bus;
+  private AutoConfiguration configuration;
 
-    private InMemServer inMemServer;
-    private Discovery discovery;
+  private InMemServer inMemServer;
+  private Discovery discovery;
+  private boolean fail = false;
 
-    public InMemTransport(
-            AutoConfiguration configuration,
-            EventBus bus) {
-        this.bus = bus;
-        this.configuration = configuration;
+  public InMemTransport(
+    AutoConfiguration configuration,
+    EventBus bus) {
+    this.bus = bus;
+    this.configuration = configuration;
+  }
+
+  @Override
+  public boolean canConnectToService(String name) {
+    Optional<ServiceDescriptor> descriptor = discovery
+      .getServiceNamed(name);
+
+    if (!descriptor.isPresent()) return false;
+
+    return descriptor.get().getSchemes().stream().anyMatch(url -> url.equals(getUrlScheme()));
+  }
+
+  @Override
+  public void shutdown() {
+    bus.unregister(inMemServer);
+  }
+
+  @Override
+  public void start(Discovery discovery, ServerStacks serverStacks, Codecs codecs, Scheduler scheduler) throws MuonTransportFailureException {
+    this.discovery = discovery;
+    this.inMemServer = new InMemServer(configuration.getServiceName(), bus, serverStacks);
+  }
+
+  @Override
+  public String getUrlScheme() {
+    return "inmem";
+  }
+
+  public void triggerFailure() {
+    fail = true;
+    bus.post(new InMemFailureEvent());
+  }
+
+  @Override
+  public URI getLocalConnectionURI() {
+    try {
+      return new URI("inmem://" + configuration.getServiceName());
+    } catch (URISyntaxException e) {
+      throw new MuonTransportFailureException("Incorrect URI for inmem", e);
     }
+  }
 
-    @Override
-    public boolean canConnectToService(String name) {
-        Optional<ServiceDescriptor> descriptor = discovery
-                .getServiceNamed(name);
+  @Override
+  public ChannelConnection<MuonOutboundMessage, MuonInboundMessage> openClientChannel(String serviceName, String protocol) {
+    if(fail) {
+      log.warn("InMemTransport is in failure mode. Attempted to open a new client channel, sending {}", TransportEvents.CONNECTION_FAILURE);
+      return new ChannelConnection<MuonOutboundMessage, MuonInboundMessage>() {
 
-        if (!descriptor.isPresent()) return false;
+        private ChannelFunction<MuonInboundMessage> function;
 
-        return descriptor.get().getSchemes().stream().anyMatch( url -> url.equals(getUrlScheme()));
-    }
-
-    @Override
-    public void shutdown() {
-        bus.unregister(inMemServer);
-    }
-
-    @Override
-    public void start(Discovery discovery, ServerStacks serverStacks, Codecs codecs, Scheduler scheduler) throws MuonTransportFailureException {
-        this.discovery = discovery;
-        this.inMemServer = new InMemServer(configuration.getServiceName(), bus, serverStacks);
-    }
-
-    @Override
-    public String getUrlScheme() {
-        return "inmem";
-    }
-
-    public void triggerFailure() {
-      bus.post(new InMemFailureEvent());
-    }
-
-    @Override
-    public URI getLocalConnectionURI() {
-        try {
-            return new URI("inmem://" + configuration.getServiceName());
-        } catch (URISyntaxException e) {
-            throw new MuonTransportFailureException("Incorrect URI for inmem", e);
+        @Override
+        public void receive(ChannelFunction<MuonInboundMessage> function) {
+          this.function = function;
         }
-    }
 
-    @Override
-    public ChannelConnection<MuonOutboundMessage, MuonInboundMessage> openClientChannel(String serviceName, String protocol) {
-        return new DefaultInMemClientChannelConnection(serviceName, protocol, bus);
+        @Override
+        public void send(MuonOutboundMessage message) {
+          function.apply(MuonMessageBuilder.fromService("local")
+            .protocol(protocol)
+            .step(TransportEvents.CONNECTION_FAILURE)
+            .payload(new byte[0])
+            .contentType("application/json")
+            .operation(MuonMessage.ChannelOperation.closed)
+            .buildInbound()
+          );
+        }
+
+        @Override
+        public void shutdown() {
+
+        }
+      };
     }
+    return new DefaultInMemClientChannelConnection(serviceName, protocol, bus);
+  }
 }
--- muon-core/src/test/groovy/io/muoncore/inmem/transport/InMemTransportSpec.groovy
+++ muon-core/src/test/groovy/io/muoncore/inmem/transport/InMemTransportSpec.groovy
@@ -1,4 +1,5 @@
 package io.muoncore.inmem.transport
+
 import com.google.common.eventbus.EventBus
 import io.muoncore.Discovery
 import io.muoncore.channel.support.Scheduler
@@ -7,42 +8,70 @@ import io.muoncore.config.AutoConfiguration
 import io.muoncore.memory.transport.InMemClientChannelConnection
 import io.muoncore.memory.transport.InMemTransport
 import io.muoncore.memory.transport.OpenChannelEvent
+import io.muoncore.message.MuonInboundMessage
 import io.muoncore.protocol.ServerStacks
+import io.muoncore.transport.TransportEvents
+import io.muoncore.transport.TransportFailureSpec
 import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
 
 class InMemTransportSpec extends Specification {
 
-    Codecs codecs = Mock(Codecs) {
-        encode(_, _) >> new Codecs.EncodingResult(new byte[0], "application/json")
+  Codecs codecs = Mock(Codecs) {
+    encode(_, _) >> new Codecs.EncodingResult(new byte[0], "application/json")
+  }
+
+  def "returns client channel connection on demand."() {
+    def eventbus = new EventBus()
+    def serverStacks = Mock(ServerStacks)
+
+    def transport = new InMemTransport(new AutoConfiguration(serviceName: "tombola"), eventbus)
+    transport.start(Mock(Discovery), serverStacks, codecs, new Scheduler())
+
+    when:
+    def ret = transport.openClientChannel("tombola", "simple")
+
+    then:
+    ret instanceof InMemClientChannelConnection
+  }
+
+  def "transport listens on event bus for OpenChannelEvents. opens channels in response"() {
+    def eventbus = new EventBus()
+    def serverStacks = Mock(ServerStacks)
+    def clientConnection = Mock(InMemClientChannelConnection)
+
+    def transport = new InMemTransport(new AutoConfiguration(serviceName: "tombola"), eventbus)
+    transport.start(Mock(Discovery), serverStacks, codecs, new Scheduler())
+
+    when:
+    eventbus.post(new OpenChannelEvent(
+      "tombola", "simple", clientConnection))
+
+    then:
+    1 * serverStacks.openServerChannel("simple")
+  }
+
+  def "when failurs is triggered, channel connections are immediately closed with a transport failure error"() {
+    def eventbus = new EventBus()
+    def serverStacks = Mock(ServerStacks)
+
+    def transport = new InMemTransport(new AutoConfiguration(serviceName: "tombola"), eventbus)
+    transport.start(Mock(Discovery), serverStacks, codecs, new Scheduler())
+    transport.triggerFailure()
+
+    when:
+    def ret = transport.openClientChannel("tombola", "simple")
+
+    MuonInboundMessage fail
+
+    ret.receive {
+      fail = it
     }
 
-    def "returns client channel connection on demand."() {
-        def eventbus = new EventBus()
-        def serverStacks = Mock(ServerStacks)
-
-        def transport = new InMemTransport(new AutoConfiguration(serviceName: "tombola"), eventbus)
-        transport.start(Mock(Discovery), serverStacks, codecs, new Scheduler())
-
-        when:
-        def ret = transport.openClientChannel("tombola", "simple")
-
-        then:
-        ret instanceof InMemClientChannelConnection
+    then:
+    new PollingConditions().eventually {
+      fail
+      fail.step == TransportEvents.CONNECTION_FAILURE
     }
-
-    def "transport listens on event bus for OpenChannelEvents. opens channels in response"() {
-        def eventbus = new EventBus()
-        def serverStacks = Mock(ServerStacks)
-        def clientConnection = Mock(InMemClientChannelConnection)
-
-        def transport = new InMemTransport(new AutoConfiguration(serviceName: "tombola"), eventbus)
-        transport.start(Mock(Discovery), serverStacks, codecs, new Scheduler())
-
-        when:
-        eventbus.post(new OpenChannelEvent(
-                "tombola", "simple", clientConnection))
-
-        then:
-        1 * serverStacks.openServerChannel("simple")
-    }
+  }
 }
